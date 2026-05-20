2a3937dab24075a289474bf9cc6212f0fd33b3aa
Niloygh <niloyniloy843@gmail.com>
2026-05-20 12:29:06 +0600

    font add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/component/Navbar";
 import Footer from "@/component/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -22,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en" data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased `}
+      className={`${inter.className} h-full antialiased `}
     >
       <body className="min-h-full flex flex-col">
         <Navbar />
